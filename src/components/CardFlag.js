e530f825c1d9f4606627dc11c00c51a137f97a05
ldmoore <lucasdmo@unc.edu>
2024-10-31 15:02:19 -0400

    Updates

--- src/components/CardFlag.js
+++
@@ -1,9 +0,0 @@
-import "./cardflag.css";
-
-export default function CardFlag({ text }) {
-    return (
-        <div className="CardFlag App-list-column">
-            <p>{text}</p>
-        </div>
-    );
-}
